1775283c3a587d4e8ab3d76f55aade29539a1d23
Ran <ran_zhao@brown.edu>
2026-07-27 11:04:04 -0400

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://cloud-machine-111.github.io',
+    base: '/lobbying_networks_site',
+});
